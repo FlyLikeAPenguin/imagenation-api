b652067851d825f4143d2f1f63342c7a5fdde011
Joe Thornton <joe.clifford.thornton@gmail.com>
2025-01-05 13:50:43 +0900

    Addedd basic image gen functionality to FastAPI

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
-
+import helpers
 app = FastAPI()
 
+      
 
 @app.get("/")
 def read_index():
     return {"Hello": "World"}
+
+@app.get("/generate_image")
+def generate_image(prompt: str):
+    return helpers.generate_image(prompt)
