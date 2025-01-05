621e659c069bfd9635ab12ed4cf9a9d90a0a2a0c
Joe Thornton <joe.clifford.thornton@gmail.com>
2025-01-05 15:54:09 +0900

    Added basic rate limiting

--- src/main.py
+++ src/main.py
@@ -1,11 +1,29 @@
-from fastapi import FastAPI, HTTPException
+import redis.asyncio as redis
+
+from contextlib import asynccontextmanager
+
+from decouple import config
+from fastapi import FastAPI, HTTPException, Depends
+from fastapi_limiter import FastAPILimiter
+from fastapi_limiter.depends import RateLimiter
 from pydantic import BaseModel
+
 import helpers
-app = FastAPI()
+
+REDIS_URL = config("REDIS_URL")
+
+@asynccontextmanager
+async def lifespan(_:FastAPI):
+    redis_conn = redis.from_url(REDIS_URL)
+    await FastAPILimiter.init(redis_conn)
+    yield
+    await redis_conn.close()
+
+app = FastAPI(lifespan=lifespan)
 
       
 
-@app.get("/")
+@app.get("/", dependencies=[Depends(RateLimiter(times=1, seconds=5))])
 def read_index():
     return {"Hello": "World"}
 
@@ -14,7 +32,7 @@ class ImageGenerationRequest(BaseModel):
     prompt: str
 
 
-@app.post("/generate")
+@app.post("/generate", dependencies=[Depends(RateLimiter(times=1, seconds=5))])
 def generate_image(data: ImageGenerationRequest):
     try:
         pred_result = helpers.generate_image(data.prompt)
